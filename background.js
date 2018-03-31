bffb4fceebbb9dbe0c758c1017ec82ca6d8bf824
Cormac Neeson <neesonch@tcd.ie>
2018-03-31 15:08:15 +0100

    feat: icon greyed out unless active bookmark found

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
-// STATUS: Icon will change on click. Next step: add conditional logic to enable/disable tracking and change icon accordingly
-// TODOS: Create Git Repo -- Debounce scrolling to resolve error? Unload approach not workable (scrolling seems to be working now that reference to function is passed in instead of deifing the function inline) -- Sanitize urls to remove parameters when saving/retrieving -- Examine bug, scroll events not firing when changing tabs (have to refresh tab for scroll events to work - may have just been test steps issue) -- Enable/disable bookmark tracking on click of icon
+// STATUS: Icon greyed out unless bookmark found. Next step: add conditional logic to enable/disable tracking and change icon accordingly
+// TODOS: Debounce scrolling to resolve error? Unload approach not workable (scrolling seems to be working now that reference to function is passed in instead of deifing the function inline) -- Sanitize urls to remove parameters when saving/retrieving -- Examine bug, scroll events not firing when changing tabs (have to refresh tab for scroll events to work - may have just been test steps issue) -- Enable/disable bookmark tracking on click of icon
 // BACKLOG: Add popup UI to view currently tracked bookmarks
+
+const GREY_ICON_PATH = "assets/icons/bm_grey.png";
+const BLUE_ICON_PATH = "assets/icons/bm_blue.png";
 
 chrome.browserAction.onClicked.addListener(function(tab){
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
@@ -8,10 +11,6 @@ chrome.browserAction.onClicked.addListener(function(tab){
 		var message = {"message": "clicked_browser_action"};
 		chrome.tabs.sendMessage(activeTab.id, message, responseHandler);
 		console.log(message);
-		chrome.browserAction.setIcon({
-            path: "assets/icons/bm_grey.png",
-            tabId: activeTab.id
-        });
 		}
 	)
 });
@@ -24,9 +23,12 @@ chrome.runtime.onMessage.addListener(
 				// Null check
 				if(!(Object.keys(item).length === 0 && item.constructor === Object)){
 					chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-						var activeTab = tabs[0];
-						var message = {"message": "bookmark_found", "bookmarkAt": item[url].scrollPosition};
+						const activeTab = tabs[0];
+						const pageData = item[url];
+						const message = {"message": "bookmark_found", "bookmarkAt": pageData.scrollPosition};
 						chrome.tabs.sendMessage(activeTab.id, message, responseHandler);
+						const iconPath = pageData.bookmarkEnabled ? BLUE_ICON_PATH : GREY_ICON_PATH;
+						setIcon(activeTab.id, iconPath);
 					});
 				}
 			});
@@ -43,7 +45,14 @@ function responseHandler(response){
 	//console.log(response);
 }
 
-function saveBookmark(url, scrollPosition){
+setIcon = (tabId, iconPath) => {
+		chrome.browserAction.setIcon({
+        path: iconPath,
+        tabId: tabId
+    });
+}
+
+saveBookmark = (url, scrollPosition) => {
 	let objectToSave = {};
 	objectToSave[url] = {"scrollPosition": scrollPosition, "bookmarkEnabled": true};
 	console.log(objectToSave);	//DEBUG
